696a073d65c05f95403dca43b627f0ab2f2ec2c1
2022KirillMaximov <102306817+2022KirillMaximov@users.noreply.github.com>
2023-07-07 16:44:49 +0200

    1 task not done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,56 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+function createMarkuo(arr) {
+    return arr.map(({ preview, original, description }) =>
+        
+        `<li class="gallery__item">
+  <a class="gallery__link" href="">
+    <img 
+    class="gallery__image"
+    src="${preview}" 
+    data-source="${original}"
+    alt="${description}">
+  </a>
+</li>`
+
+    ).join('')
+}
+gallery.insertAdjacentHTML('afterbegin', createMarkuo(galleryItems));
+
+
+gallery.addEventListener('click', handlerClickGallery)
+
+function handlerClickGallery(evt) {
+    evt.preventDefault();
+    if (evt.target.classList.contains('gallery__image')) {
+        console.log('gallery__image', evt.target)
+    }
+
+
+    const instance = basicLightbox.create(
+        `<img src="${evt.target.dataset.source}" 
+        width='1300' height='900' >`,
+        {
+            onShow: handlerEscapeModal,
+            onClose: handlerEscapeModal,
+        })
+    instance.show()
+
+        function handlerEscapeModal(evt) {
+    if (evt.code === "Escape") {
+        instance.close()
+    }
+}
+}
+
+
+
+
+
+
+
+
+
