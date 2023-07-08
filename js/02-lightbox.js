823717414a8f5a36ce7d1aede88e71333ccf4eb1
2022KirillMaximov <102306817+2022KirillMaximov@users.noreply.github.com>
2023-07-08 17:07:05 +0200

    task 2 not complete

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+function createMarkuo(arr) {
+    return arr.map(({  preview, original, description }) =>
+        
+        `<li class="gallery__item">
+  <a class="gallery__link"  href="large-image.jpg" "${original}">
+    <img 
+    class="gallery__image"
+    src=  "${preview}" 
+    alt="${description}">
+  </a>
+</li>`
+
+    ).join('')
+}
+gallery.insertAdjacentHTML('afterbegin', createMarkuo(galleryItems));
+
+
+
+const lightbox = new SimpleLightbox('.gallery a',{
+  
+  captionsData: `alt`, captionDelay: 250,
+})
+lightbox.show()
+
