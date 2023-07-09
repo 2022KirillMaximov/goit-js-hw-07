f0567d299143a4e13ec7bde063b8aed39d16aae3
2022KirillMaximov <102306817+2022KirillMaximov@users.noreply.github.com>
2023-07-09 12:54:51 +0200

    2 task done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -9,7 +9,7 @@ function createMarkuo(arr) {
     return arr.map(({  preview, original, description }) =>
         
         `<li class="gallery__item">
-  <a class="gallery__link"  href="large-image.jpg" "${original}">
+  <a class="gallery__link"  href="${original}">
     <img 
     class="gallery__image"
     src=  "${preview}" 
@@ -23,9 +23,9 @@ gallery.insertAdjacentHTML('afterbegin', createMarkuo(galleryItems));
 
 
 
-const lightbox = new SimpleLightbox('.gallery a',{
+const lightbox = new SimpleLightbox(".gallery a", {
   
   captionsData: `alt`, captionDelay: 250,
 })
-lightbox.show()
+
 
